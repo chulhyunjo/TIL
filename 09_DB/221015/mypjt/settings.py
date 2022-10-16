b0e3d49cc0bb8cad08666b674424da5e423d4007
chulhyunjo <chaoman5@naver.com>
2022-10-16 15:21:17 +0900

    TIL 수정

--- 09_DB/221015/mypjt/settings.py
+++ 09_DB/221015/mypjt/settings.py
@@ -15,13 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o@e+3v2_=h@o+5t^3_%m#h8^t1eg#$)!_^i%-b0f4p+m%uqv!2'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
