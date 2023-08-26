03e54e246e30c5b4a015bee262b4ca7f201c91fa
chulhyunjo <chaoman5@naver.com>
2023-08-26 16:53:00 +0900

    TIL 업로드

--- 03_algorithm/2308/0812/소프티어 1번.py
+++ 03_algorithm/2308/0812/소프티어 1번.py
@@ -1,25 +1,19 @@
-import time
-start = time.time()
+import sys
+input = sys.stdin.readline
 nums_dict = dict()
 
-# N, M = map(int,input().split())
-# nums = list(map(int,input().split()))
-
-N = 50000; M = 2000000
-nums = list(range(10, 50000*10+1, 10))
-Q = list(range(10000, 10000*200000+1, 10000))
-
+N, M = map(int,input().split())
+nums = list(map(int,input().split()))
 nums.sort()
 for i in range(1, N+1):
     nums_dict[nums[i-1]] = i
 
 # for _ in range(M):
 #     m = int(input())
-for m in Q:
+for m in nums:
     idx = nums_dict.get(m, -1)
     if idx == -1:
         print(0)
     else:
         answer = (N-idx) * (idx-1)
         print(answer)
-print(time.time() - start)
--- 03_algorithm/2308/0812/소프티어1번 이분탐색.py
+++ 03_algorithm/2308/0812/소프티어1번 이분탐색.py
@@ -1,45 +1,40 @@
 import sys
-import time
-start = time.time()
-
-f = open("input.txt", 'r', encoding='UTF8')
-N, M = map(int,f.readline().split())
-nums = list(map(int,f.readline().split()))
+input = sys.stdin.readline
+N, M = map(int,input().split())
+nums = list(map(int,input().split()))
 nums.sort()
 
 
+for _ in range(M):
+    m = int(input().strip())
+    s = 0
+    e = N-1
+    answer = 0
+    if nums[s] >= m or nums[e] <= m:
+        print(0)
+        continue
+    while s <= e:
+        mid = (s+e) // 2
+        now = nums[mid]
+        if now == m :
+            answer = (N-mid-1) * mid
+            break
+        elif now > m:
+            e = mid - 1
+        elif now < m:
+            s = mid + 1
+
+    print(answer)
+
+# nums_dict = dict()
+# for i in range(1, N+1):
+#     nums_dict[nums[i-1]] = i
+#
 # for _ in range(M):
 #     m = int(f.readline().strip())
-#     s = 0
-#     e = N-1
-#     answer = 0
-#     if nums[s] >= m or nums[e] <= m:
+#     idx = nums_dict.get(m, -1)
+#     if idx == -1:
 #         print(0)
-#         continue
-#     while s <= e:
-#         mid = (s+e) // 2
-#         now = nums[mid]
-#         if now == m :
-#             answer = (N-mid-1) * mid
-#             break
-#         elif now > m:
-#             e = mid - 1
-#         elif now < m:
-#             s = mid + 1
-
-    # print(answer)
-
-nums_dict = dict()
-for i in range(1, N+1):
-    nums_dict[nums[i-1]] = i
-
-for _ in range(M):
-    m = int(f.readline().strip())
-    idx = nums_dict.get(m, -1)
-    if idx == -1:
-        print(0)
-    else:
-        answer = (N-idx) * (idx-1)
-        print(answer)
-
-print(time.time() - start)
+#     else:
+#         answer = (N-idx) * (idx-1)
+#         print(answer)
